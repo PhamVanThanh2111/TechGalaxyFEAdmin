8e0b6a3563c5b0864376cf69a6c867b52432bb8a
Kha <minhkha20031996@gmail.com>
2024-11-18 23:07:53 +0700

    hi

--- src/main/java/iuh/fit/se/techgalaxy/frontend/admin/configs/RestClientConfig.java
+++ src/main/java/iuh/fit/se/techgalaxy/frontend/admin/configs/RestClientConfig.java
@@ -1,12 +1,9 @@
 package iuh.fit.se.techgalaxy.frontend.admin.configs;
 
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 public class RestClientConfig implements WebMvcConfigurer {
