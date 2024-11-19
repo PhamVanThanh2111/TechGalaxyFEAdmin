5a790b2e8632de8b7f4a25a9945f9e4dce1fffb2
PhamVanThanh <phamtrung21112003@gmail.com>
2024-11-19 12:51:20 +0700

    update Trademark

--- src/main/java/iuh/fit/se/techgalaxy/frontend/admin/services/TrademarkService.java
+++ src/main/java/iuh/fit/se/techgalaxy/frontend/admin/services/TrademarkService.java
@@ -6,5 +6,9 @@ import iuh.fit.se.techgalaxy.frontend.admin.dto.response.TrademarkResponse;
 public interface TrademarkService {
     DataResponse<TrademarkResponse> findAll();
 
+    DataResponse<TrademarkResponse> findById(String id);
 
+    DataResponse<TrademarkResponse> save(String name);
+
+    DataResponse<Object> delete(String id);
 }
