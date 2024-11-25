667506a19c70d93daa430509f22541a41a355003
PhamVanThanh <phamtrung21112003@gmail.com>
2024-11-25 18:28:19 +0700

    create order detail

--- src/main/java/iuh/fit/se/techgalaxy/frontend/admin/services/OrderDetailService.java
+++ src/main/java/iuh/fit/se/techgalaxy/frontend/admin/services/OrderDetailService.java
@@ -1,8 +1,11 @@
 package iuh.fit.se.techgalaxy.frontend.admin.services;
 
+import iuh.fit.se.techgalaxy.frontend.admin.dto.request.OrderDetailRequest;
 import iuh.fit.se.techgalaxy.frontend.admin.dto.response.DataResponse;
 import iuh.fit.se.techgalaxy.frontend.admin.dto.response.OrderDetailResponse;
 
 public interface OrderDetailService {
     DataResponse<OrderDetailResponse> getOrderDetail(String orderId);
+
+    DataResponse<OrderDetailResponse> createOrderDetail(OrderDetailRequest orderDetailRequest);
 }
